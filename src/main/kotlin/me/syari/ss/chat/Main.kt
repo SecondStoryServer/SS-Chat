44a60ed83c9f4db8eae43541afab2c73d1562311
sya-ri <sya79lua@gmail.com>
2020-05-31 15:47:58 +0900

    サーバー起動時にDiscordについて出力するように変更

--- src/main/kotlin/me/syari/ss/chat/Main.kt
+++ src/main/kotlin/me/syari/ss/chat/Main.kt
@@ -2,20 +2,29 @@ package me.syari.ss.chat
 
 import me.syari.ss.core.auto.Event
 import me.syari.ss.core.auto.OnEnable
+import me.syari.ss.core.message.ConsoleLogger
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main: JavaPlugin() {
     companion object {
         internal lateinit var chatPlugin: JavaPlugin
 
+        internal lateinit var chatLogger: ConsoleLogger
+
         internal var enableDiscord = false
     }
 
     override fun onEnable() {
         chatPlugin = this
+        chatLogger = ConsoleLogger(this)
         enableDiscord = server.pluginManager.isPluginEnabled("SS-Discord")
         OnEnable.register(ConfigLoader, CommandCreator)
         Event.register(this, ChatEventListener)
-        if(enableDiscord) Event.register(this, DiscordHook)
+        if(enableDiscord) {
+            Event.register(this, DiscordHook)
+            chatLogger.info("Discord を有効にしました")
+        } else {
+            chatLogger.info("Discord を無効にしました")
+        }
     }
 }
