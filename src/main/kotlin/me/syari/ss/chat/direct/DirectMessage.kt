736a39cf68defc3bfe8173d3a00556c791b17db2
sya-ri <sya79lua@gmail.com>
2020-06-01 23:46:12 +0900

    CommandSenderから前回のDM相手を取得するプロパティを追加

--- src/main/kotlin/me/syari/ss/chat/direct/DirectMessage.kt
+++ src/main/kotlin/me/syari/ss/chat/direct/DirectMessage.kt
@@ -1,5 +1,18 @@
 package me.syari.ss.chat.direct
 
-object DirectMessage {
+import org.bukkit.command.CommandSender
 
+object DirectMessage {
+    private val lastDirectMessagePartnerMap = mutableMapOf<DirectMessageSender, DirectMessageSender>()
+
+    var CommandSender.lastDirectMessagePartner
+        get() = DirectMessageSender.from(this)?.let { lastDirectMessagePartnerMap[it] }
+        set(value) {
+            val directMessageSender = DirectMessageSender.from(this) ?: return
+            if(value != null){
+                lastDirectMessagePartnerMap[directMessageSender] = value
+            } else {
+                lastDirectMessagePartnerMap.remove(directMessageSender)
+            }
+        }
 }
--- src/main/kotlin/me/syari/ss/chat/direct/DirectMessageSender.kt
+++ src/main/kotlin/me/syari/ss/chat/direct/DirectMessageSender.kt
@@ -3,6 +3,7 @@ package me.syari.ss.chat.direct
 import me.syari.ss.core.Main.Companion.console
 import me.syari.ss.core.player.UUIDPlayer
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 
 interface DirectMessageSender {
     val sender: CommandSender?
@@ -15,5 +16,15 @@ interface DirectMessageSender {
     object Console: DirectMessageSender {
         override val sender: CommandSender?
             get() = console
+    }
+
+    companion object {
+        fun from(commandSender: CommandSender): DirectMessageSender? {
+            return when(commandSender){
+                is org.bukkit.entity.Player -> Player(UUIDPlayer(commandSender))
+                is ConsoleCommandSender -> Console
+                else -> null
+            }
+        }
     }
 }
